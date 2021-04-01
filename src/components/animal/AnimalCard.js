b7b34f280320c070f04c470df3472b5f49fe4bea
bvinson23 <brandonvinson23@gmail.com>
2021-04-01 15:13:48 -0500

    finished commenting my code to help me understand what is happening

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./Animal.css"
 
+//function to make an animal card
 export const AnimalCard = () => (
     <section className="animal">
         <h3 className="animal__name">Doodles</h3>
