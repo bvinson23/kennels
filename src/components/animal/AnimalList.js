abf2e13c456de29a3c434a4b265a7d5f833b68ed
bvinson23 <brandonvinson23@gmail.com>
2021-04-02 11:35:12 -0500

    updated to use the fetch call

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,6 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react";
+import { getAllAnimals, getAnimalById } from "../../modules/AnimalManager";
 
 export const AnimalList = () => {
+    const getAnimals = () => {
+        return getAllAnimals.then(animalsFromAPI => {
+            console.log(animalsFromAPI)
+        });
+    };
+
+    useEffect(() => {
+        getAnimals();
+    }, []);
+
     return (
         <div className="container-cards">
             We'll put some animals here eventually...
