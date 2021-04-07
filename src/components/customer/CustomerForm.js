87d4c41b5ce304e02ee4e61ea4a1bbda2681d747
bvinson23 <brandonvinson23@gmail.com>
2021-04-07 15:16:56 -0500

    functionality for adding a customer works!

---
+++ src/components/customer/CustomerForm.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { addCustomer } from '../../modules/CustomerManager';
+import './CustomerForm.css'
+
+export const CustomerForm = () => {
+
+    const [customer, setCustomer] = useState({
+        name: "",
+        address: ""
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const history = useHistory();
+
+    //Controlled component
+    const handleControlledInputChange = (event) => {
+        const newCustomer = { ...customer }
+        let selectedVal = event.target.value
+        if (event.target.id.includes("Id")) {
+            selectedVal = parseInt(selectedVal)
+        }
+
+        newCustomer[event.target.id] = selectedVal
+        setCustomer(newCustomer)
+    }
+
+    const handleClickSaveCustomer = (event) => {
+        event.preventDefault()
+
+        addCustomer(customer)
+            .then(() => history.push("/customers"))
+    }
+
+    return (
+        <form className="customerForm">
+			<h2 className="customerForm__title">New Customer</h2>
+			<fieldset>
+				<div className="form-group">
+					<label htmlFor="name">Customer name:</label>
+					<input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Customer name" value={customer.name} />
+				</div>
+			</fieldset>
+			<fieldset>
+				<div className="form-group">
+					<label htmlFor="address">Customer address:</label>
+					<input type="text" id="address" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Address" value={customer.address} />
+				</div>
+			</fieldset>
+			<button className="btn btn-primary"
+				onClick={handleClickSaveCustomer}>
+				Save Customer
+          </button>
+		</form>
+    )
+}
